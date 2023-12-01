b78c37e9f4e5eb01cccc9095cbf7174eb85948c3
KEGL--TOURNEIX Noemie <Noemie.Kegl--Tourneix@bordeaux-inp.fr>
2023-12-01 16:50:11 +0100

    test

--- extraction.m
+++ extraction.m
@@ -1,6 +1,26 @@
-function [extractedImg] = extraction(invH, sourceImg, extractedImg)
+function [extractedImg] = extraction(invH, sourceImg, h,w)
     
-    [h, w] = size(extractedImg);
+    % [sourceH, sourceW] = size(sourceImg);
+    % 
+    % % Créez une grille de coordonnées pour l'image extraite
+    % [X, Y] = meshgrid(1:w, 1:h);
+    % 
+    % % Appliquez la transformation (inversée) à toutes les coordonnées en une seule opération
+    % coords = invH * [X(:)'; Y(:)'; ones(1, h*w)];
+    % 
+    % % Convertissez les coordonnées transformées en coordonnées d'indice
+    % x2 = floor(coords(3, :) ./ coords(1, :));
+    % y2 = floor(coords(3, :) ./ coords(2, :));
+    % 
+    % % Initialisez l'image extraite avec des zéros
+    % extractedImg = zeros(h, w);
+    % 
+    % % Remplissez l'image extraite avec les pixels valides de l'image source
+    % for i = 1:length(x2)
+    %     if x2(i) >= 1 && x2(i) <= sourceW && y2(i) >= 1 && y2(i) <= sourceH
+    %         extractedImg(Y(i), X(i)) = sourceImg(y2(i), x2(i));
+    %     end
+    % end
     
     for x=1:w
 	    for y=1:h
@@ -8,7 +28,9 @@ function [extractedImg] = extraction(invH, sourceImg, extractedImg)
             M2 = invH * [x; y; 1];
             x2 = floor(M2(1)/M2(3));
             y2 = floor(M2(2)/M2(3));
+
 		    extractedImg(y,x) = sourceImg(y2,x2);
+
 	    end
     end
 
--- main_no.m
+++ main_no.m
@@ -3,7 +3,7 @@ clear; close all; clc;
 %% Initialisation
 
 % Charger l'image
-img = imread('tab.jpg');
+img = imread('images.jpeg');
 
 % initialisation des tailles du rectangle
 w = 500;
@@ -26,32 +26,49 @@ y = [coords(1,2), coords(2,2), coords(3,2), coords(4,2)];
 rectangle_x = [1, 1, h, h];
 rectangle_y = [1, w, w, 1];
 
-% Appeler la fonction "identification" pour obtenir la matrice d'homographie
-H = identification(x, y, rectangle_x, rectangle_y);
+H = homographie_arm(x, y, rectangle_x, rectangle_y);
 
-% Transformer les coins du rectangle initial en fonction de l'homographie
-transformed_rectangle = [rectangle_x; rectangle_y; ones(1, 4)];
-transformed_rectangle = H * transformed_rectangle;
+% transformed_rectangle = [rectangle_x; rectangle_y; ones(1, 4)];
+% transformed_rectangle = H * transformed_rectangle;
+% 
+% transformed_rectangle = transformed_rectangle ./ transformed_rectangle(3, :);
+% 
+% transformed_x = transformed_rectangle(1, :);
+% transformed_y = transformed_rectangle(2, :);
+% 
+% xmin = min(transformed_x);
+% xmax = max(transformed_x);
+% ymin = min(transformed_y);
+% ymax = max(transformed_y);
+% 
+% region_width = xmax - xmin + 1;
+% region_height = ymax - ymin + 1;
+% 
+% extracted_region = imcrop(img, [xmin, ymin, region_width - 1, region_height - 1]);
+% 
+% 
+% % Affichage
+% 
+% disp('Coordonnées des clics :');
+% disp(coords);
+% 
+% % Afficher la région extraite
+% figure;
+% imshow(extracted_region);
+% title('Contenu du rectangle transformé');
 
-% Diviser par la coordonnée Z pour obtenir les coordonnées homogènes
-transformed_rectangle = transformed_rectangle ./ transformed_rectangle(3, :);
 
-% Extraire les coordonnées x et y du rectangle transformé
-transformed_x = transformed_rectangle(1, :);
-transformed_y = transformed_rectangle(2, :);
 
-% Extraire la région du rectangle transformé depuis l'image d'origine
-xmin = min(transformed_x);
-xmax = max(transformed_x);
-ymin = min(transformed_y);
-ymax = max(transformed_y);
+% Créer une transformation géométrique à partir de la matrice d'homographie
+tform = projective2d(H);
 
-% Obtenir les dimensions de la région du rectangle transformé
-region_width = xmax - xmin + 1;
-region_height = ymax - ymin + 1;
+% % Définir les dimensions du rectangle parallèle aux axes
+% outputWidth = h;
+% outputHeight = w;
 
-% Utiliser la fonction imcrop pour extraire la région du rectangle transformé
-extracted_region = imcrop(img, [xmin, ymin, region_width - 1, region_height - 1]);
+% Appliquer l'homographie pour extraire le contenu du quadrangle
+% quadrangle = imwarp(img, tform, 'OutputView', imref2d([w, h]));
+quadrangle = extraction(inv(H), img, h, w);
 
 
 %% Affichage
@@ -59,31 +76,8 @@ extracted_region = imcrop(img, [xmin, ymin, region_width - 1, region_height - 1]
 disp('Coordonnées des clics :');
 disp(coords);
 
-% Afficher la région extraite
+% Afficher le contenu du quadrangle extrait
 figure;
-imshow(extracted_region);
-title('Contenu du rectangle transformé (sans imwarp, méthode simple)');
-
-
-
-% % Créer une transformation géométrique à partir de la matrice d'homographie
-% tform = projective2d(H);
-% 
-% % Définir les dimensions du rectangle parallèle aux axes
-% outputWidth = h;
-% outputHeight = w;
-% 
-% % Appliquer l'homographie pour extraire le contenu du quadrangle
-% quadrangle = imwarp(img, tform, 'OutputView', imref2d([outputHeight, outputWidth]));
-% 
-% 
-% %% Affichage
-% 
-% disp('Coordonnées des clics :');
-% disp(coords);
-% 
-% % Afficher le contenu du quadrangle extrait
-% figure;
-% imshow(quadrangle);
+imshow(quadrangle);
 % title('Poster extrait avec homographie');
 
