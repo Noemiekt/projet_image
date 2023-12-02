3d3a84d5c2486644aa56f673699f5272c6882016
Sallmone Armela <asallmone007@bordeaux-inp.fr>
2023-12-02 13:29:47 +0100

    main armelouche

---
+++ main_homography_extraction.m
@@ -0,0 +1,41 @@
+clear; close all;
+
+%% Sallmone Armela, Kegl Noémie, Mony Alexandra 
+
+% Main script to perform image extraction using homography
+
+% Load the source image 
+sourceImg = imread('picture_arm.jpeg');
+
+% Display the source image ans selct 4 points
+imshow(sourceImg);
+title('Select 4 points on the image');
+% Points on the source image
+[XU, YU] = ginput(4); 
+
+% Destination points
+width = 200; 
+height = 200;
+XR = [1, width, width, 1]; % dest points in X
+YR = [1, 1, height, height]; % dest points in Y
+
+% Homography matrix 
+H = homographie(XU, YU, XR, YR);
+
+% Invert H
+invH = inv(H);
+
+% Extract image
+extractedImg = extraction(invH,sourceImg, height, width);
+
+% Display extracted image 
+figure;
+imshow(extractedImg);
+title('Extracted Image');
+
+
+ 
+
+
+
+
