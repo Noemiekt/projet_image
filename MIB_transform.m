eb91122f76190c9f22cbfc7fe730e6910429015c
KEGL--TOURNEIX Noemie <Noemie.Kegl--Tourneix@bordeaux-inp.fr>
2023-12-19 16:46:36 +0100

    caca

--- MIB_transform.m
+++ MIB_transform.m
@@ -3,12 +3,12 @@ function [MIB2] = MIB_transform(MIB1,H)
     X = [MIB1.xmin, MIB1.xmin,MIB1.xmax, MIB1.xmax];
     Y = [MIB1.ymin, MIB1.ymax,MIB1.ymin, MIB1.ymax];
 
-    
+
 
     XU = zeros(1,4);
     YU = zeros(1,4);
 
-    
+
     for i = 1:4
         % Appliquer la matrice d'homographie inverse aux coordonnées transformées
         homog_coord = H* [X(i); Y(i); 1];
@@ -17,38 +17,43 @@ function [MIB2] = MIB_transform(MIB1,H)
         YU(i) = homog_coord(2) / homog_coord(3);
 
     end
-     
+
     MIB2.xmin = min(floor(XU));
     MIB2.ymin = min(floor(YU));
 
     MIB2.xmax = max(ceil(XU));
     MIB2.ymax = max(ceil(YU));
 
-    
 
-    h = MIB2.xmax - MIB2.xmin + 1;
 
-    MIB2.I = zeros(h,w);
+    w = MIB2.xmax - MIB2.xmin + 1;
+    h = MIB2.ymax - MIB2.ymin + 1;
+
+    MIB2.I = zeros(h,w,3);
     MIB2.M = zeros(h,w);
-
+    
+    
     for x = MIB2.xmin:MIB2.xmax
-        for y = MIB2.xmax:MIB2.ymax
-
+        
+        for y = MIB2.ymin:MIB2.ymax
+            
             homog_coord = inv(H)* [x; y; 1];
 
             XU = homog_coord(1) / homog_coord(3);
             YU = homog_coord(2) / homog_coord(3);
 
-            if (((XU<=MIB1.xmax)&&(XU>=MIB1.xmin) && ((YU<=MIB1.ymax)&&(YU>=MIB1.ymin))))
+            if (((YU<=MIB1.xmax)&&(YU>=MIB1.xmin) && ((XU<MIB1.ymax)&&(XU>MIB1.ymin))))
 
                 MIB2.M(x - MIB2.xmin +1 , y - MIB2.ymin +1) = 1;
-                MIB2.I(x - MIB2.xmin +1 , y - MIB2.ymin +1, :) = MIB1.I(YU - MIB1.xmin +1, XU - MIB1.ymin +1, :);
-    
+                MIB2.I(x - MIB2.xmin +1, y - MIB2.ymin +1, :) = MIB1.I(round(YU - MIB1.xmin), round(XU - MIB1.ymin) +1, :);
+              
+
             end
-            
+
 
         end
     end
+
 
 end
 
--- main_mosaique.m
+++ main_mosaique.m
@@ -1,77 +1,7 @@
-% clear; close all;
+clear; close all;
+dbstop if error;
 % 
-% %% Sallmone Armela, Kegl Noémie, Mony Alexandra 
-% 
-% % Main script to perform image extraction using homography
-% 
-% % Load the source image 
-% image1 = imread('images/image1.png');
-% image2 = imread('images/image2.png');
-% 
-% figure;
-% subplot(1, 2, 1);
-% imshow(image1);
-% title('Image 1');
-% 
-% subplot(1, 2, 2);
-% imshow(image2);
-% title('Image 2');
-% 
-% % n = 4; % Nombre de points à capturer pour chaque image
-% % 
-% % % Capturez 4 points de la première image
-% % subplot(1, 2, 1);
-% % [x1, y1] = ginput(n);
-% % 
-% % % Capturez 4 points de la deuxième image
-% % subplot(1, 2, 2);
-% % [x2, y2] = ginput(n);
-% % 
-% % % Affichez les points capturés sur la première image
-% % subplot(1, 2, 1);
-% % hold on;
-% % scatter(x1, y1, 'r', 'filled');
-% % hold off;
-% % 
-% % % Affichez les points capturés sur la deuxième image
-% % subplot(1, 2, 2);
-% % hold on;
-% % scatter(x2, y2, 'r', 'filled');
-% % hold off;
-% 
-% % Homography matrix 
-% x1=[290, 646,646,290];
-% x2=[11, 361,361,11];
-% y1=[330,330,420,420];
-% y2=[9,9,100,100];
-% 
-% H=homographie(x1, y1, x2, y2);
-% 
-% 
-% % Dimensions de l'image extraite
-% n = size(image2, 1);
-% m = size(image2, 2);
-% 
-% 
-% image1rec = image1;
-% 
-% MIB_img1 = ItoMIB(image1);
-% MIB_img2 = MIB_transform(MIB_img1, H);
-% 
-% MIB(1) = MIB_img1;
-% MIB(2) = MIB_img2;
-% MIB = ceil(MIB(1), MIB(2));
-% MIB_out = MIB_fusion(MIB);
-% 
-% 
-% 
-% 
-% % Afficher l'image résultante
-% figure;
-% imshow(MIB_out);
-% title('Image avec la portion extraite réintégrée');
-% 
-% clear; close all;
+
 
 %% Sallmone Armela, Kegl Noémie, Mony Alexandra 
 
@@ -124,7 +54,12 @@ H1to2=homographie(x1, y1, x2, y2);
 MIB1=ItoMIB(image1);
 MIB2=ItoMIB(image2);
 
-MIB2_trans=MIB_transform(MIB1,H1to2);
+MIB2_trans= MIB_transform(MIB1,H1to2);
+
+% Afficher l'image resultante
+figure;
+imagesc(uint8(MIB2.I));
+title('Image avec la portion extraite réintégrée');
 
 maCellule = cell(1, 2);
 
