a36566dbfcd130173cd433191af4b5a75ef6c3e4
MONY ALEXANDRA <amony001@enseirb-matmeca.fr>
2023-12-20 14:54:38 +0100

    votre ancien code

---
+++ old/identification.m
@@ -0,0 +1,21 @@
+function [H, Hform] =  identification(XU, YU, XR, YR)
+    % Estimer l'homographie
+    tform = fitgeotrans([XU', YU'], [XR', YR'], 'projective');
+
+    % Obtenir la matrice d'homographie
+    Hform = tform.T;
+    
+    A = zeros(8, 8);
+    B = zeros(8, 1);
+    
+    for i = 1:4
+        A(2*i-1, :) = [XU(i), YU(i), 1, 0, 0, 0, -XU(i)*XR(i), -YU(i)*XR(i)];
+        A(2*i, :) = [0, 0, 0, XU(i), YU(i), 1, -XU(i)*YR(i), -YU(i)*YR(i)];
+        B(2*i-1) = XR(i);
+        B(2*i) = YR(i);
+    end
+    
+    X = A\B;
+    H = reshape([X; 1], 3, 3);
+
+end
---
+++ old/main_mosaique.m
@@ -0,0 +1,91 @@
+clear; close all;
+dbstop if error;
+% 
+
+
+%% Sallmone Armela, Kegl Noémie, Mony Alexandra 
+
+% Main script to perform image extraction using homography
+
+% Load the source image 
+image1 = imread('images/image1.png');
+image2 = imread('images/image2.png');
+
+figure;
+subplot(1, 2, 1);
+imshow(image1);
+title('Image 1');
+
+subplot(1, 2, 2);
+imshow(image2);
+title('Image 2');
+
+% n = 4; % Nombre de points à capturer pour chaque image
+% 
+% % Capturez 4 points de la première image
+% subplot(1, 2, 1);
+% [x1, y1] = ginput(n);
+% 
+% % Capturez 4 points de la deuxième image
+% subplot(1, 2, 2);
+% [x2, y2] = ginput(n);
+% 
+% % Affichez les points capturés sur la première image
+% subplot(1, 2, 1);
+% hold on;
+% scatter(x1, y1, 'r', 'filled');
+% hold off;
+% 
+% % Affichez les points capturés sur la deuxième image
+% subplot(1, 2, 2);
+% hold on;
+% scatter(x2, y2, 'r', 'filled');
+% hold off;
+
+% Homography matrix 
+x1=[290, 646,646,290];
+x2=[11, 361,361,11];
+y1=[330,330,420,420];
+y2=[9,9,100,100];
+
+H1to2=homographie(x1, y1, x2, y2);
+
+
+MIB1=ItoMIB(image1);
+MIB2=ItoMIB(image2);
+
+MIB2_trans= MIB_transform(MIB1,H1to2);
+
+% Afficher l'image resultante
+figure;
+imagesc(uint8(MIB2.I));
+title('Image avec la portion extraite réintégrée');
+
+maCellule = cell(1, 2);
+
+MIBin=cell(1);
+
+MIBin{1}=MIB1;
+MIBin{2}=MIB2_trans;
+
+MIBout=MIB_fusion(MIBin);
+
+
+
+% Afficher l'image résultante
+figure;
+imshow(MIBout.I);
+title('Image avec la portion extraite réintégrée');
+
+
+
+
+
+
+
+
+
+
+
+ 
+
